560b13810edad2d436b727fe77a010acbc690a2d
Devmarcuz <otunbamarcusademola@gmail.com>
2025-06-08 08:31:18 +0100

    added password validation

---
+++ src/components/ActionTexts.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+import { motion } from "framer-motion";
+import "@/styles/ActionCard.css";
+
+type Props = {
+  tagContents: Array<string>;
+  rmBorder?: boolean;
+  lightMode?: boolean;
+};
+
+const ActionTexts = ({ tagContents, rmBorder, lightMode }: Props) => {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setActiveIndex((prev) => (prev + 1) % tagContents.length);
+    }, 1500);
+
+    return () => clearInterval(interval);
+  }, [tagContents.length]);
+
+  return (
+    <div
+      className={`action-card ${rmBorder && "rm-border"} ${
+        lightMode && "light-mode"
+      }`}
+    >
+      <div className="contain">
+        {tagContents.map((content, index) => (
+          <motion.h3
+            key={index}
+            initial={false}
+            animate={{
+              opacity:
+                activeIndex === index
+                  ? 1
+                  : index === (activeIndex + 1) % tagContents.length
+                  ? 0.5
+                  : 0.2,
+            }}
+            transition={{
+              duration: 0.8,
+              ease: [0.25, 1, 0.5, 1],
+            }}
+          >
+            {content}
+          </motion.h3>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ActionTexts;
---
+++ src/components/NewHeroSection.tsx
@@ -0,0 +1,11 @@
+import "@/styles/NewHeroSection.css";
+
+const NewHeroSection = () => {
+  return (
+    <div className="new-hero-section">
+      <section></section>
+    </div>
+  );
+};
+
+export default NewHeroSection;
